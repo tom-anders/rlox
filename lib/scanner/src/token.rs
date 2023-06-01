842150869233589f7018374cdd3b2a02892ccf56
tom-anders <13141438+tom-anders@users.noreply.github.com>
2023-06-02 00:41:54 +0200

    feat: implement tokens up to 4.6.1

---
+++ lib/scanner/src/token.rs
@@ -0,0 +1,57 @@
+#[derive(Debug)]
+pub struct Token<'a> {
+    pub data: TokenData<'a>,
+    pub lexeme: &'a str,
+    pub line: usize,
+}
+
+#[derive(Debug)]
+pub enum TokenData<'a> {
+    // Single-character tokens.
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    // One or two character tokens.
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // Literals.
+    Identifier,
+    StringLiteral(&'a str),
+    Number(f64),
+
+    // Keywords.
+    And,
+    Claas,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+    Eof,
+}
+
