351f47c32c605c89d299e016603a4dad6c36d354
tom-anders <13141438+tom-anders@users.noreply.github.com>
2023-06-08 14:55:43 +0200

    refactor(scanner): add dedicated Cursor class, rewrite scanner into Iterator

--- lib/scanner/src/token.rs
+++ lib/scanner/src/token.rs
@@ -1,16 +1,43 @@
-use std::fmt::Display;
+use std::{fmt::Display};
+
+use source::Cursor;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Token<'a> {
     pub data: TokenData<'a>,
-    pub lexeme: &'a str,
-    pub line: usize,
-    pub col_start: usize,
+    pub start: Cursor<'a>,
+    pub end: Cursor<'a>,
+    // pub lexeme: &'a str,
+    // pub line: usize,
+    // pub col_start: usize,
+}
+
+impl<'a> Token<'a> {
+    // TODO store the cursors/range instead?
+    pub fn new(data: TokenData<'a>, start: Cursor<'a>, end: Cursor<'a>) -> Token<'a> {
+        Self {
+            data,
+            start,
+            end,
+        }
+    }
+
+    pub fn lexeme(&self) -> &str {
+        self.start.slice_until(&self.end)
+    }
+
+    pub fn line(&self) -> usize {
+        self.start.line()
+    }
+
+    pub fn col(&self) -> usize {
+        self.start.col()
+    }
 }
 
 impl Display for Token<'_> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.lexeme)
+        write!(f, "{}", self.lexeme())
     }
 }
 
