8ec3d73f04df29f1e67094814fe34e6f9b006b89
tom-anders <13141438+tom-anders@users.noreply.github.com>
2023-08-09 20:03:28 +0200

    test: add zoo benchmark from the book

--- lib/vm/benches/bench.rs
+++ lib/vm/benches/bench.rs
@@ -15,15 +15,36 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         Vm::new().run_source(source, &mut output).unwrap();
     }));
 
-    c.bench_function("basic class stuff", |b| b.iter(|| {
+    c.bench_function("zoo example from the book", |b| b.iter(|| {
         let source = r#"
-            class Pair { }
+            class Zoo {
+              init() {
+                this.aardvark = 1;
+                this.baboon   = 1;
+                this.cat      = 1;
+                this.donkey   = 1;
+                this.elephant = 1;
+                this.fox      = 1;
+              }
+              ant()    { return this.aardvark; }
+              banana() { return this.baboon; }
+              tuna()   { return this.cat; }
+              hay()    { return this.donkey; }
+              grass()  { return this.elephant; }
+              mouse()  { return this.fox; }
+            }
 
-            var pair = Pair();
-            pair.first = 1;
-            pair.second = 2;
-            print pair;
-            print pair.first + pair.second;
+            var zoo = Zoo();
+            var sum = 0;
+            var start = clock();
+            while (sum < 1000) {
+              sum = sum + zoo.ant()
+                        + zoo.banana()
+                        + zoo.tuna()
+                        + zoo.hay()
+                        + zoo.grass()
+                        + zoo.mouse();
+            }
         "#;
         let mut output = Vec::new();
         Vm::new().run_source(source, &mut output).unwrap();
