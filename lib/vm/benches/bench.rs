82c0ffa4ee143f99cbd8c75fb2c04eacbad6ba96
tom-anders <13141438+tom-anders@users.noreply.github.com>
2024-02-09 15:26:43 +0100

    feat: add all benchmarks from the book repo as criterion benchmarks

--- lib/vm/benches/bench.rs
+++ lib/vm/benches/bench.rs
@@ -1,59 +1,23 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use vm::Vm;
 
-pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fib", |b| {
-        b.iter(|| {
-            let source = r#"
-            fun fib(n) {
-                if (n <= 1) return n;
-                return fib(n - 2) + fib(n - 1);
-            }
-
-            print fib(10);
-        "#;
-            let mut output = Vec::new();
-            Vm::new().run_source(source, &mut output).unwrap();
-        })
-    });
-
-    c.bench_function("zoo example from the book", |b| {
-        b.iter(|| {
-            let source = r#"
-            class Zoo {
-              init() {
-                this.aardvark = 1;
-                this.baboon   = 1;
-                this.cat      = 1;
-                this.donkey   = 1;
-                this.elephant = 1;
-                this.fox      = 1;
-              }
-              ant()    { return this.aardvark; }
-              banana() { return this.baboon; }
-              tuna()   { return this.cat; }
-              hay()    { return this.donkey; }
-              grass()  { return this.elephant; }
-              mouse()  { return this.fox; }
-            }
-
-            var zoo = Zoo();
-            var sum = 0;
-            var start = clock();
-            while (sum < 1000) {
-              sum = sum + zoo.ant()
-                        + zoo.banana()
-                        + zoo.tuna()
-                        + zoo.hay()
-                        + zoo.grass()
-                        + zoo.mouse();
-            }
-        "#;
-            let mut output = Vec::new();
-            Vm::new().run_source(source, &mut output).unwrap();
-        })
-    });
+pub fn book_repo_benchmarks(c: &mut Criterion) {
+    for benchmark in std::fs::read_dir(format!(
+        "{}/../../ext/craftinginterpreters/test/benchmark/",
+        env!("CARGO_MANIFEST_DIR")
+    ))
+    .unwrap()
+    .flatten()
+    .filter(|entry| entry.path().extension() == Some("lox".as_ref()))
+    {
+        let source = std::fs::read_to_string(benchmark.path()).unwrap();
+        c.bench_function(benchmark.file_name().to_str().unwrap(), |b| {
+            b.iter(|| {
+                Vm::new().run_source(&source, &mut Vec::new()).unwrap();
+            });
+        });
+    }
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group!(benches, book_repo_benchmarks);
 criterion_main!(benches);
